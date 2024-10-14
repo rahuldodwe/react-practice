2d5bfd6a3067f56320b53de814f378bfb9b2b41d
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-10-14 21:33:55 +0530

    Register form completed

--- 09-commmon-form/src/config/index.js
+++ 09-commmon-form/src/config/index.js
@@ -18,4 +18,35 @@ export const loginFormElements = [
         componentType: 'input',
         type: 'password',
     }
+];
+
+
+export const registerFormElements = [
+
+    {
+        name: 'name',
+        id: 'name',
+        placeholder: 'Enter your name',
+        label: 'Name',
+        componentType: 'input',
+        type: 'text',
+    },
+
+    {
+        name: 'email',
+        id: 'email',
+        placeholder: 'Enter your email',
+        label: 'Email',
+        componentType: 'input',
+        type: 'email',
+    },
+
+    {
+        name: 'password',
+        id: 'password',
+        placeholder: 'Enter your password',
+        label: 'password',
+        componentType: 'input',
+        type: 'password',
+    }
 ]
--- 09-commmon-form/src/pages/register/Register.jsx
+++ 09-commmon-form/src/pages/register/Register.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CommonForm from '../../components/common-from/CommonForm'
+import { registerFormElements } from '../../config'
+
+const initialRegisterFormData = {
+  name : '',
+  email : '',
+  password : '',
+}
 
 const Register = () => {
+
+  const [registerFormData, setRegisterFormData] = useState(initialRegisterFormData);
+
+  function handleRegisterOnSubmit(event) {
+    event.preventDefault();
+
+    console.log(registerFormData);
+
+    setRegisterFormData(initialRegisterFormData)
+    
+  }
+
+
   return (
     <div>
       <h1>Register Page</h1>
+      <CommonForm
+      formControls={registerFormElements}
+      formData={registerFormData}
+      setFormData={setRegisterFormData}
+      buttonText={"Register"}
+      onHandleSubmit={handleRegisterOnSubmit}
+      />
+
     </div>
   )
 }
